b36e741ce3fe6d15c37dc30009acd6fbe35610f0
SandrineLem <lemossandrine@gmail.com>
2021-12-08 11:19:44 +0100

    Ajout de middleware test amelioration du server.js

--- backend/app.js
+++ backend/app.js
@@ -2,9 +2,24 @@
 const express = require('express');
 //creer l'appli Express 
 const app = express();
+
+//-----Middleware (fonction)----
+app.use((req, res, next)=>{
+    console.log('Requête recue!');
+    next();//pour passer à la requete suivante 
+});
+//modification du code de la reponse status 
+app.use((req, res, next)=>{
+    res.status(201);
+    next();
+});
 //methode app.use (creer une route reponse avec un type de requete  )
-app.use((req, res)=>{
-    res.json({message: 'votre requête à bien été recue!'})
-})
+app.use((req, res, next)=>{
+    res.json({message: 'votre requête à bien été recue!'});
+    next();
+});
+app.use ((req, res)=>{
+    console.log('La réponse à été envoyée avec succès !');
+});
 //exporter l'appli
 module.exports = app;
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,53 @@
 //creation du programme qui va ecouter et repondre 
 const http = require('http');
+const { type } = require('os');
 //importer le fichier app.js 
 const app = require('./app');
-//informer sur quel port app doit fonctionner enviro ou 3000
-app.set('port', process.env.PORT || 3000);
+//--fonction qui renvoie un port valide ( en numero/chaine ) 
+const normalizePort = val =>{
+    const port = parseInt(val, 10);
+
+    if (isNaN(port)){
+        return val;
+    }
+    if (port >=0){
+        return port;
+    }
+    return false;
+};
+
+const port = normalizePort(process.env.PORT || '3000');
+app.set ('port', port);
+//--Fonction qui recherche les errors et les geres puis enregistre dans le server
+//--Ajout d'un ecouteur d'evenement 'listening' pour voir sur quel canal/ server s'exécute la console 
+const errorHandler = error => {
+    if (error.syscall !== 'listen'){
+        throw error;
+    };
+    const address = server.address();
+    const bind = typeof address ==='string' ? 'pipe' + address : 'port: ' + port;
+    switch(error.code){
+        case 'EACCES':
+            console.log(bind + 'requires elevated privileges.');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind +' is already in use.');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+};
+
+
 const server = http.createServer(app);
+
+server.on('error', errorHandler);
+server.on('listening', () =>{
+    const address = server.address();
+    const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+    console .log('listening on ' + bind); 
+});
 // ecoute et attente du serrver
-server.listen(process.env.PORT || 3000);
+server.listen(port);
